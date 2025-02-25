1b0c7088fc551abde4a61e9b982ae56126bc71fe
harshraneja <ranejaharsh@gmail.com>
2025-02-25 07:15:34 +0000

    first

---
+++ book-txt.py
@@ -0,0 +1,55 @@
+import fitz
+
+# file = fitz.open("book.pdf")
+# for page in file:
+#     text = page.get_text()
+#     print(text)
+
+# with open('yy.txt', 'w') as f:
+#     for page1 in file:
+#         f.write(page1.get_text())   
+
+# f.close()
+
+
+# file = fitz.open("book2.pdf")
+# for page1 in file:
+#     text1 = page.get_text()
+#     print(text1)
+
+# with open('yy.txt', 'a') as f:
+#     for page2 in file:
+#         f.write(page2.get_text())  
+
+
+
+
+
+
+
+
+
+
+
+# for i in file:
+#     print(i)
+#print(file[0])
+
+# page = 0
+
+# text = file[page].get_text()
+# print(text)
+
+
+def read_book(name):
+    file = fitz.open(name)
+    for page in file:
+        text = page.get_text()
+        print(text)
+
+    with open('yy.txt', 'a') as f:
+        for page1 in file:
+            f.write(page1.get_text())
+
+read_book('book.pdf')
+read_book('book2.pdf')
